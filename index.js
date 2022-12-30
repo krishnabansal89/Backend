5272d0a1e9e0ff94b35f2e793b4fb2253d9e4373
krishnabansal89 <83087345+krishnabansal89@users.noreply.github.com>
2022-12-30 20:46:32 +0530

    try -02

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const mongoose  = require('mongoose')
 const cors = require('cors')
 const connect = require('./db')
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000
 const path = require('path')
 app.use(express.json()) 
 
